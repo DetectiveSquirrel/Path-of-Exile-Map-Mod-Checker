43a4d456e427dd0c42247633c8364aca6e6cdcc7
Detective Squirrel <ratman7539@windowslive.com>
2017-04-13 17:03:43 +1200

    Giving Source

---
+++ WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.TrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(259, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 39);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(258, 211);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // TrayIcon
+            // 
+            this.TrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("TrayIcon.Icon")));
+            this.TrayIcon.Text = "notifyIcon1";
+            this.TrayIcon.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Find Map Mods";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.NotifyIcon TrayIcon;
+    }
+}
+
---
+++ WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AddClipboardFormatListener(this.Handle);    // Add our window to the clipboard's format listener list.
+
+        }
+        
+        /// <summary>
+         /// Places the given window in the system-maintained clipboard format listener list.
+         /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        /// <summary>
+        /// Removes the given window from the system-maintained clipboard format listener list.
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+
+        /// <summary>
+        /// Sent when the contents of the clipboard have changed.
+        /// </summary>
+        private const int WM_CLIPBOARDUPDATE = 0x031D;
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_CLIPBOARDUPDATE)
+            {
+                IDataObject iData = Clipboard.GetDataObject();      // Clipboard's data.
+
+                /* Depending on the clipboard's current data format we can process the data differently. 
+                 * Feel free to add more checks if you want to process more formats. */
+                if (iData.GetDataPresent(DataFormats.Text))
+                {
+                    var playSound = false;
+                
+                    string text = (string)iData.GetData(DataFormats.Text);
+
+                    //split each line of the map into string arrays
+                    string[] SplitItem = Regex.Split(text, "\r\n|\r|\n");
+                    //split each search into string arrays
+                    string[] myStrings = textBox2.Text.Split('|');
+
+                    // Only check maps from path fo exile to avoid misshaps
+                    if (text.Contains("Map Tier: "))
+                    {
+                        for (var i = 0; i < SplitItem.Length; i++)
+                        {
+                            for (var j = 0; j < myStrings.Length; j++)
+                            {
+                                if (SplitItem[i].ToLower().Contains(myStrings[j].ToLower()))
+                                {
+                                    richTextBox1.Text += SplitItem[i] + "\r";
+                                    playSound = true;
+                                }
+                            }
+                        } 
+                    }
+                    
+                    // if found string then play sound ONLY once, actually tested against multi search and boy was that sound i heard retarded :D
+                    if (playSound)
+                    {
+                        // play notify.wav (can replace with what ever sound file you want after building i guess)
+                        System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Content\notify.wav");
+                        player.Play();
+                        playSound = false;
+                    }
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Save shit
+            Properties.Settings.Default.WindowsLocation = this.Location;
+            Properties.Settings.Default.ModLine = textBox2.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // load shit
+            this.Location = Properties.Settings.Default.WindowsLocation;
+            textBox2.Text = Properties.Settings.Default.ModLine;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Save text on every input because ive had things crash in the past and just like to be exactly where ive left off
+            Properties.Settings.Default.ModLine = textBox2.Text;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
